25d45e6bfc865b2973195a3d634b2ac607bb5ff0
omerbd21 <omerbendavid21@gmail.com>
2023-06-29 17:08:47 +0000

    changed tests to not include specific namespace names like budaka
    Signed-off-by: omerbd21 <omerbendavid21@gmail.com>

--- test/e2e/cappPlacement-test/level02Script.sh
+++ test/e2e/cappPlacement-test/level02Script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-clusterset=`kubectl get placement nesharim -n default -o jsonpath='{.spec.clusterSets[0]}'`
+clusterset=`kubectl get placement placement-2nd -n ocp-kuba-gader -o jsonpath='{.spec.clusterSets[0]}'`
 echo $clusterset
-clusters=`kubectl get managedclusters -l cluster.open-cluster-management.io/clusterset=default -o jsonpath='{range .items[*]}{.metadata.name}{" "}{end}'`
+clusters=`kubectl get managedclusters -l cluster.open-cluster-management.io/clusterset=$clusterset -o jsonpath='{range .items[*]}{.metadata.name}{" "}{end}'`
 clusters_strings=$(echo $clusters | tr ' ' '|')
 # | sed 's/ /| /g'
 fixed_clusters_strings+=$clusters_strings
 
-kubectl assert exist-enhanced capp --field-selector metadata.name=capp-with-placement,status.applicationLinks.site=~$fixed_clusters_strings -n budaka
+kubectl assert exist-enhanced capp --field-selector metadata.name=capp-with-placement,status.applicationLinks.site=~$fixed_clusters_strings -A
