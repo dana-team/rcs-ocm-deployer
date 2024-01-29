b8e42e623b447b5773111a88c1338f6f27bb8290
mzeevi <meytar80@gmail.com>
2024-01-29 20:04:33 +0000

    tst
    Signed-off-by: mzeevi <meytar80@gmail.com>

--- solutions/local-quickstart.sh
+++ solutions/local-quickstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-bash $(shell pwd)/solutions/setup-kind-clusters.sh
-bash $(shell pwd)/solutions/ocm-join-clusters.sh $1
-bash $(shell pwd)/solutions/setup-rcs-deployer.sh $1
+bash $(pwd)/solutions/setup-kind-clusters.sh
+bash $(pwd)/solutions/ocm-join-clusters.sh $1
+bash $(pwd)/solutions/setup-rcs-deployer.sh $1
